dc2e522e687b1b4e9f6cb150f148de04adba2edf
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2020-12-05 21:46:17 +0000

    Day 04 - Part 2

--- day-04/day_04.py
+++ day-04/day_04.py
@@ -37,9 +37,88 @@ def validatorV1(passport: dict) -> bool:
     return True
 
 def validatorV2(passport: dict) -> bool:
+    if (
+        validatorV1(passport)
+        and validate_byr(passport['byr'])
+        and validate_ecl(passport['ecl'])
+        and validate_eyr(passport['eyr'])
+        and validate_hcl(passport['hcl'])
+        and validate_hgt(passport['hgt'])
+        and validate_iyr(passport['iyr'])
+        and validate_pid(passport['pid'])
+        ):
+        return True
+    else:
+        return False
+
+def validate_year(value: str, min_val: int, max_val: int) -> bool:
+    try:
+        value = int(value)
+    except:
+        print("Year Not String")
+        return False
+    if min_val <= value and value <= max_val:
+        print("Year Valid")
+        return True
+    print("Year Not Valid")
+    return False
     
 
-def part1():
+def validate_byr(value: str) -> bool:
+    return validate_year(value, 1920, 2002)
+
+def validate_ecl(value: str) -> bool:
+    valid = ["amb","blu","brn","gry","grn","hzl","oth"]
+    if value in valid:
+        print("Eye Colour Valid")
+        return True
+    else:
+        print("Eye Colour Not Valid")
+        return False
+
+def validate_eyr(value: str) -> bool:
+    return validate_year(value, 2020, 2030)
+
+def validate_hcl(value: str) -> bool:
+    pattern = r'#([0-9a-fA-F]){6}'
+    if re.fullmatch(pattern, value):
+        print("Hair Colour Valid")
+        return True
+    else:
+        print("Hair Colour Not Valid")
+        return False
+
+def validate_hgt(value: str) -> bool:
+    pattern = r'(\d+)(in|cm)'
+    match = re.match(pattern, value)
+    if match:
+        if ((
+                match.group(2) == 'in' 
+                and int(match.group(1)) >= 59 
+                and int(match.group(1)) <= 76
+            ) or (
+                match.group(2) == 'cm' 
+                and int(match.group(1)) >= 150 
+                and int(match.group(1)) <= 193
+            )):
+                print("Height Valid")
+                return True
+    print("Height Not Valid")
+    return False
+
+def validate_iyr(value: str) -> bool:
+    return validate_year(value, 2010, 2020)
+
+def validate_pid(value: str) -> bool:
+    pattern = r'\d{9}'
+    if re.fullmatch(pattern, value):
+        print("PID Valid")
+        return True
+    else:
+        print("PID Not Valid")
+        return False
+
+def load() -> list:
     file_name = "input.txt"
     lines = open(file_name, "r").read()
 
@@ -48,8 +127,12 @@ def part1():
     
     #remove extra line breaks
     lines = re.sub(r'\n\n',r'\n',lines)
+    return lines.split('\n')
+
+def part1():
+    lines = load()
     ids = 0
-    for line in lines.split('\n'):
+    for line in lines:
         print(line)
         passport = fieldExtractor(line)
         if validatorV1(passport):
@@ -57,4 +140,16 @@ def part1():
 
     print(ids)
 
+def part2():
+    lines = load()
+    ids = 0
+    for line in lines:
+        print(line)
+        passport = fieldExtractor(line)
+        if validatorV2(passport):
+            ids += 1
+
+    print(ids)
+
 part1()
+part2()
