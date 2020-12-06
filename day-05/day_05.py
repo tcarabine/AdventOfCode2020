acea7d1a05c7d5f82a798e3bc196c88424991efd
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2020-12-06 00:33:50 +0000

    Day 5

---
+++ day-05/day_05.py
@@ -0,0 +1,33 @@
+import io
+
+def part1():
+    file_name = "input.txt"
+    lines = open(file_name, "r").readlines()
+    seats = []
+
+    for line in lines:
+        line = line.replace("B","1").replace("F","0")
+        line = line.replace("R","1").replace("L","0")
+        seats.append(int(line,2))
+    
+    seats.sort()
+    print(seats[-1])
+
+def part2():
+    file_name = "input.txt"
+    lines = open(file_name, "r").readlines()
+    seats = []
+
+    for line in lines:
+        line = line.replace("B","1").replace("F","0")
+        line = line.replace("R","1").replace("L","0")
+        seats.append(int(line,2))
+    
+    seats.sort()
+    
+    for i in range(len(seats) - 1):
+        if seats[i+1] - seats[i] == 2:
+            print(seats[i] + 1)
+
+part1()
+part2()
